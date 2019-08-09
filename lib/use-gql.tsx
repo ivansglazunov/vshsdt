01054f91141741658c019431fec49c3851b44809
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-08-09 13:25:32 +0300

    better links view

--- lib/use-gql.tsx
+++ lib/use-gql.tsx
@@ -4,11 +4,6 @@ import { useQuery, useSubscription, useMutation as _useMutation } from '@apollo/
 export { useQuery, useSubscription };
 
 export function useGql(query, options = {}): { error: any; data: any; loading: boolean; } {
-  if (!process.browser) {
-    const qr = useQuery(Gql`{ ${query} }`, { ssr: true, ...options });
-    console.log(qr);
-    return { data: qr.data, loading: qr.loading, error: qr.error };
-  }
   const qr = useQuery(Gql`{ ${query} }`, { ssr: true, ...options });
   const sr = useSubscription(Gql`subscription { ${query} }`, options);
   if (sr.loading) return { data: qr.data, loading: qr.loading, error: qr.error };
--- pages/links.tsx
+++ pages/links.tsx
@@ -12,25 +12,113 @@ if (process.browser) {
 const GET_NODES = gql`
   nodes {
     id
+    props {
+      nodeId
+      id
+      passport_passwords {
+        id
+        propId
+      }
+      sessions {
+        id
+        propId
+      }
+      types {
+        id
+        propId
+      }
+    }
+    links_by_source {
+      id
+      sourceId
+      targetId
+    }
+    links_by_target {
+      id
+      sourceId
+      targetId
+    }
   }
 `;
 
-const GET_LINKS = gql`
-  links {
-    id
-    sourceId
-    targetId
+const parseLink = (input, links, _links, nodes) => {
+  for (let l = 0; l < input.length; l++) {
+    const link = input[l];
+    if (!_links[link.id]) {
+      _links[link.id] = true;
+      nodes.push({
+        id: `l${link.id}`,
+        group: link.__typename,
+      });
+      links.push({
+        id: `l${link.id}`,
+        source: `n${link.sourceId}`,
+        target: `l${link.id}`,
+        group: `${link.__typename}-source`,
+      });
+      links.push({
+        id: `l${link.id}`,
+        source: `l${link.id}`,
+        target: `n${link.targetId}`,
+        group: `${link.__typename}-target`,
+      });
+    }
   }
-`;
+};
+
+const parseProp = (prop, rel, links, _links, nodes) => {
+  for (let p = 0; p < prop[rel].length; p++) {
+    const pr = prop[rel][p];
+    nodes.push({
+      id: `${rel}${pr.id}`,
+      group: pr.__typename,
+    });
+    links.push({
+      id: `pr${pr.id}`,
+      source: `${rel}${pr.id}`,
+      target: `p${prop.id}`,
+      group: pr.__typename,
+    });
+  }
+};
 
 export default () => {
-  const nodesQ = useGql(GET_NODES);
-  const linksQ = useGql(GET_LINKS);
+  const query = useGql(GET_NODES);
+
+  const nodes: { id: string, group: string }[] = [];
+  const _links: { [id: string]: boolean } = {};
+  const links: { id: string, group: string, source: string, target: string }[] = [];
+  
+  const ns = _.get(query, 'data.nodes');
+  if (ns) {
+    for (let n = 0; n < ns.length; n++) {
+      const node = ns[n];
+      nodes.push({
+        id: `n${node.id}`,
+        group: node.__typename,
+      });
+      parseLink(node.links_by_source, links, _links, nodes);
+      parseLink(node.links_by_target, links, _links, nodes);
+      for (let p = 0; p < node.props.length; p++) {
+        const prop = node.props[p];
+        nodes.push({
+          id: `p${prop.id}`,
+          group: prop.__typename,
+        });
+        links.push({
+          id: `p${prop.id}`,
+          source: `p${prop.id}`,
+          target: `n${prop.nodeId}`,
+          group: prop.__typename,
+        });
+        parseProp(prop, 'passport_passwords', links, _links, nodes);
+        parseProp(prop, 'sessions', links, _links, nodes);
+        parseProp(prop, 'types', links, _links, nodes);
+      }
+    }
+  }
 
   const [{ width, height }, setSize] = useState({ width: 100, height: 100 });
-
-  const nodes = _.map(_.get(nodesQ, 'data.nodes', []), node => ({ node, id: node.id, group: node.__typename }));
-  const links = _.map(_.get(linksQ, 'data.links', []), link => ({ link, id: link.id, source: link.sourceId, target: link.targetId }));
 
   return <div style={{ position: 'absolute', left: 0, top: 0, width: '100%', height: '100%' }}>
     <ReactResizeDetector handleWidth handleHeight onResize={(width, height) => {
@@ -48,7 +136,7 @@ export default () => {
       linkDirectionalArrowRelPos={1}
       linkCurvature={0.25}
       nodeCanvasObject={(node, ctx, globalScale) => {
-        const label = `n${node.id}`;
+        const label = node.id;
         const fontSize = 12/globalScale;
         ctx.font = `${fontSize}px Sans-Serif`;
         const textWidth = ctx.measureText(label).width;
