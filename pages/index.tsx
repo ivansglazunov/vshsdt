0cdff5e34220fc8a07c96cba4727ffbce45ac96f
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-08-09 13:32:24 +0300

    fix repo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,38 @@
-import { Canvas } from 'react-three-fiber';
-import Sandbox from '../components/Sandbox';
-import '../components/Sandbox/styles.css';
+import { useGql, gql } from '../lib/use-gql';
 
-export const Index = () => {
-  return (
-      <Canvas camera={{ position: [0, 0, 15] }}>
-        <Sandbox />
-      </Canvas>
-  );
+const GET_NODES = gql`
+nodes {
+  id
+  props {
+    nodeId
+    id
+    passport_passwords {
+      id
+      propId
+    }
+    sessions {
+      id
+      propId
+    }
+    types {
+      id
+      propId
+    }
+  }
+  links_by_source {
+    id
+    sourceId
+    targetId
+  }
+  links_by_target {
+    id
+    sourceId
+    targetId
+  }
 }
+`;
 
-export default Index;
+export default () => {
+  const query = useGql(GET_NODES);
+  return <pre>{JSON.stringify(query, null, 2)}</pre>;
+};
