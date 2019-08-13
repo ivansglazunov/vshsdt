a4591875004acf2ee2c3339e38f5233255c8a40b
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-08-13 18:21:17 +0300

    new config concept

--- config.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  POSTGRES:
-    'postgres://vguxyqbknrwqmy:c4993ed4d0cd4bf08a5c29d9591cfdbfd8de10d51d665e58d1d2780e114a5665@ec2-107-21-102-221.compute-1.amazonaws.com:5432/ddt9oc6kbkpkjn',
-  HASURA_ADMIN_SECRET: '7777',
-  HASURA: 'isg-hasura-lerny.herokuapp.com',
-  GRAPHQL: 'isg-hasura-lerny.herokuapp.com/v1/graphql',
-  STRATEGIES: '88.99.24.138:4900',
-};
