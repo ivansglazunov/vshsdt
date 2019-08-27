99134ded69b4c25248cafedb58090ef0d27a1ad4
Ivan S Glazunov <ivansglazunov@gmail.com>
2019-08-28 02:45:28 +0300

    better funtion

--- migrations/0.0.0_70_links_lists_functions.ts
+++ migrations/0.0.0_70_links_lists_functions.ts
@@ -7,30 +7,39 @@ export const FUNCTION_INSERT = `
     currentLink RECORD;
     parentsCount INT;
   BEGIN
-    SELECT COUNT(*) INTO parentsCount FROM
-    "links" as parentLinks
-    WHERE
-    "targetId" = NEW.sourceId;
-    RETURN NEW;
-
-    IF parentsCount > 0 THEN
+    SELECT DISTINCT ll1."nodeId" FROM
+    (
       SELECT
-      pLLIs.listId,
-      pLLIs.listId,
-      pLLIs.listId,
-      pLLIs.listId,
+      lli0."listId",
+      lli0."nodeId",
+      lli0."depth"
       FROM
-      "links" as pLs,
-      "links_lists" as pLLs,
-      "links_lists_items" as pLLIs
+      "links_lists" as ll0,
+      "links_lists_items" as lli0
       WHERE
-      pLs.targetId = NEW.sourceId AND
-      pLLs.linkId = pLs.id AND
-      pLLIs.linkId = pLs.id AND
-      pLLIs.listId = pLLs.id
-    ELSE
-    END IF;
-  END; $trigger$ language 'plpgsql';
+      ll0."nodeId" = 4 AND
+      lli0."listId" = ll0."id"
+    ) as p0,
+    "links_lists_items" as lli1,
+    "links_lists" as ll1
+    WHERE
+    (
+      lli1."depth" = p0."depth" AND
+      lli1."nodeId" = p0."nodeId"
+    ) AND
+    ll1."id" = lli1."listId" AND
+    ll1."nodeId" NOT IN (
+      SELECT
+      lli0."nodeId"
+      FROM
+      "links_lists" as ll0,
+      "links_lists_items" as lli0
+      WHERE
+      ll0."nodeId" = 4 AND
+      lli0."listId" = ll0."id"
+    );
+  END;
+  $trigger$ language 'plpgsql';
 `;
 export const FUNCTION_DELETE = `
   DROP FUNCTION on-links-insert-function;
